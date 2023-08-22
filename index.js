173815c1d46c4545027ddb04dd33bf8b5f05fd05
faris-amer <farisamer569@gmail.com>
2023-08-22 16:02:19 -0400

    Added boilerplate bot code and package requirements

---
+++ index.js
@@ -0,0 +1,16 @@
+// Require necessary classes form discord.js
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to discord with client's token
+
+client.login(token);
